50d961c378d1cfb3f63a1b3fa8f46b42704ba006
Emma Macey <macey26@hotmail.com>
2017-12-27 22:14:37 +1100

    Adding the CSS Tardis and JavaScript Guessing game

---
+++ README.md
@@ -0,0 +1,5 @@
+The DR Who Game
+
+This is a game that I started when learning conditional statements in Javascript. I wanted to reinforce what I had learned, so I came up with the idea of creating a game that would ask a series of questions about The 12 Doctors of Doctor who, to guess their favorite Dr. The Javascript may seem simple and could also been done with functions, loops or a range of other ways to complete this, but because I am new to Javascript I decided to stick to if/else statements. I also wanted to create the TARDIS using only HTML and CSS, so I could familiarise myself with CSS. 
+
+This is a work in progess can't wast to see how it turns out.
---
+++ js/index.js
@@ -0,0 +1,105 @@
+/*	var one = confirm("Did your favorite Doctor grasp his coat collars often?");
+
+	if (one ===true){
+        
+var two =  confirm("Did he say \'hmm... whats that my boy?\'");
+		if(two === true){
+            confirm("This is the first Doctor, played by William Hartnell. Is this your favorite Doctor?");
+		} 
+		
+	}else if(true){
+		var three = confirm("Did your favorite Doctor eat fish fingers and custard?");
+		 if(three ===true){
+          var four = confirm("Did he say \'bow ties are cool?\'");
+           if(four === true) {
+           	confirm("This is the éleventh Doctor, played by Matt Smith. Is this your favorite Doctor?");	
+           }
+		 }else if(true){
+		var five = confirm("Did your favorite Doctor, have an umbrella, with a question mark for a handle?");
+		if (five === true){
+			var six = confirm("\"If we fight like animals, we die like animals\"");
+			if(six === true){
+				confirm("This is the seventh Doctor, played by Sylverster McCoy. Is this your favorite Doctor?")
+			}
+		}else if(true){
+				var seven = confirm("Did your favorite Doctor, play the recorder?");
+				if(seven === true){
+					var eight = confirm("\"Butter fingers!\"");
+					  if(eight === true){
+					  	confirm("This is the second Doctor, played by Patrick Troughton. Is this your favorite Doctor");
+					  }
+				}else if(true){
+					var nine = confirm("Did your favorite Doctor have a long scarf?");
+					if(nine ===true){
+						var ten = confirm("\"Would you like a jelly baby?\"");
+						if(ten === true){
+							confirm("This is the fourth Doctor, played by Tom Baker. Is this your favorite Doctor?");
+						}
+					} else if (true){
+						var eleven = confirm("Did your favorite Doctor play the electric Guitar?");
+						if(eleven === true){
+							var twelve = confirm("\"Ive got new kidneys!!\"");
+							if(twelve === true){
+								confirm("This is the twelfth Doctor, played by Peter Capaldi. Is this your Doctor?");
+							}
+						}  else if (true){
+						var thirteen = confirm("Did your favorite Doctor wear celery on his colar?");
+						if(thirteen === true){
+							var fourteen = confirm("\"Must dash\"");
+							if(fourteen === true){
+								confirm("This is the fifth Doctor, played by Peter Davison. Is this your favorite Doctor?");
+							}
+						}  else if (true){
+						var fifteen = confirm("Did your favorite Doctor, wear a leather jacket? ");
+						if(fifteen === true){
+							var sixteen = confirm("\"Fantastic\"");
+							if(sixteen === true){
+								confirm("This is the ninth Doctor, played by Christopher Eccleston. Is this your favorite Doctor?");
+							}
+						}  else if (true){
+						var seventeen = confirm("Did your favorite Doctor wear an outfit like Wild Bill Hickok?");
+						if(seventeen === true){
+							var eighteen = confirm("\"Who am I?\"");
+							if(eighteen=== true){
+								confirm("This is the eighth Doctor, played by Paul McGann. Is this your favorite Doctor?");
+							}
+						}  else if (true){
+						var nineteen = confirm("Did your favorite Doctor wear pinstripes, with spikey hair?");
+						if(nineteen === true){
+							var twenty = confirm("\"Allons-y!\"");
+							if(twenty === true){
+								confirm("This is the tenth Doctor, played by David Tennant. Is this your favorite Doctor?");
+							}
+						} else if (true){
+						var twentyone = confirm("Did your favorite Doctor wear a coat of many colors?");
+						if(twentyone === true){
+							var twentytwo = confirm("\"Mmm I wonder…Aha!\"");
+							if(twentytwo === true){
+								confirm("This is the sixth Doctor, played by Colin Baker. Is this your favorite Doctor?");
+							}
+						}  else if (true){
+						var twentythree = confirm("Was your favorite Doctor made to stay on earth for the majority of his lifetime?");
+						if(twentythree === true){
+							var twentyfour = confirm("Reverse the polarity");
+							if(twentyfour === true){
+								confirm("This is the third Doctor, played by Jon Pertwee. Is this your favorite Doctor?");
+							}
+						} else{
+
+		         var notSure = confirm("Not Sure about that Dr");
+					}
+					}
+
+					}
+				}
+			}
+		}
+	}
+
+	}
+}
+}
+}
+	
+	}
+*/
